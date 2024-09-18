ba481b6026528e6c3011248084a8b5326f6deb7b
Sereen <sereen.alhajjar@gmail.com>
2024-09-18 22:25:30 +0300

    adding feminine and AG feature

---
+++ tafqit.go
@@ -0,0 +1,256 @@
+package tafqit
+
+import "strings"
+
+var manazl = []string{" ", "ألف", "مليون", "مليار", "تريليون", "كوادريليون"}
+var manazlInPlural = []string{" ", "آلاف", "ملايين", "مليارات", "تريليونات", "كوادريليونات"}
+var numbers = [][]string{
+	{"صفر", "واحد", "اثنان", "ثلاث", "أربع", "خمس", "ست", "سبع", "ثمان", "تسع", "عشر"},
+	{"صفر", "واحدة", "اثنتان", "ثلاثة", "أربعة", "خمسة", "ستة", "سبعة", "ثمانية", "تسعة", "عشرة"},
+}
+
+type Options struct {
+	Feminine  bool
+	Miah      bool
+	SplitHund bool
+	Billions  bool
+	AG        bool
+}
+
+type NumberConverter struct {
+	Num int
+	Opt Options
+}
+
+func handleAG(index int, AG bool) string {
+	if AG {
+		return numbers[0][index] + "ين"
+	}
+	return numbers[0][index] + "ون"
+}
+func handleFeminine(index int, feminine bool, AG bool) string {
+	if feminine {
+		if index == 1 {
+			return numbers[1][index]
+		}
+		if index == 2 {
+			if AG {
+				return "اثنتين"
+			}
+			return "اثنتان"
+		}
+		return numbers[0][index]
+	}
+
+	if index == 1 {
+		return numbers[0][index]
+	}
+	if index == 2 {
+		if AG {
+			return "اثنين"
+		}
+		return "اثنان"
+	}
+	return numbers[1][index]
+}
+func (cnv *NumberConverter) MakeTens(index int) string {
+	if index == 0 {
+		return " "
+	}
+	if index == 1 {
+		return "عشرة"
+	}
+	if index == 2 {
+		// that's because 20 is عشرين or عشرون it's have a 10 prefix
+		return handleAG(10, cnv.Opt.AG)
+	}
+	return handleAG(index, cnv.Opt.AG)
+}
+func (cnv *NumberConverter) MakeOneDigitNum(num int) string {
+	if num == 0 {
+		return " "
+	}
+	return handleFeminine(num, cnv.Opt.Feminine, cnv.Opt.AG)
+}
+func (cnv *NumberConverter) MakeTwoDigitNum(num int) string {
+	if num%10 == 0 {
+		return cnv.MakeTens(num / 10)
+	}
+	if num/10 == 1 {
+		if num%10 == 1 {
+			if cnv.Opt.Feminine {
+				return "احدى عشرة"
+			}
+			return "أحد عشر"
+		}
+		if num%10 == 2 {
+			if cnv.Opt.Feminine {
+				if cnv.Opt.AG {
+					return "اثنتي عشرة"
+				}
+				return "اثنتا عشرة"
+			} else {
+				if cnv.Opt.AG {
+					return "اثني عشر"
+				}
+				return "اثنا عشر"
+			}
+		}
+		ten := numbers[0][10]
+		if cnv.Opt.Feminine {
+			ten = numbers[1][10]
+		}
+		return handleFeminine(num%10, cnv.Opt.Feminine, cnv.Opt.AG) + string(" ") + ten
+	}
+	return handleFeminine(num%10, cnv.Opt.Feminine, cnv.Opt.AG) + string(" ") + string("و") + cnv.MakeTens(num/10)
+}
+
+func (cnv *NumberConverter) MakeThreeDigitNum(num int) string {
+	var hundred string
+	if num/100 == 1 {
+		hundred = "مئة"
+	} else if num/100 == 2 {
+		hundred = "مئتان"
+		if cnv.Opt.AG {
+			hundred = "مئتين"
+		}
+	} else {
+		hundred = handleFeminine(num/100, cnv.Opt.Feminine, cnv.Opt.AG) + string(" ") + string("مئة")
+	}
+	if countsDigits(num%100) == 2 {
+		return hundred + string(" ") + string("و") + cnv.MakeTwoDigitNum(num%100)
+	} else if countsDigits(num%100) == 1 && num%100 != 0 {
+		return hundred + string(" ") + string("و") + cnv.MakeOneDigitNum(num%100)
+	} else {
+		return hundred
+	}
+
+}
+func countsDigits(num int) int {
+	if num == 0 {
+		return 1
+	}
+	counts := 0
+	for {
+		if num == 0 {
+			break
+		}
+		counts++
+		num /= 10
+
+	}
+	return counts
+}
+func extractLastNDigit(num, n int) int {
+	var digits []int
+	ans := 0
+	for i := 0; i < n; i++ {
+		digits = append(digits, num%10)
+		num /= 10
+	}
+	for i := n - 1; i >= 0; i-- {
+		ans *= 10
+		ans += digits[i]
+	}
+	return ans
+}
+
+func (cnv *NumberConverter) ReturnBase(num int) string {
+	digits := countsDigits(num)
+	if digits == 1 {
+		return cnv.MakeOneDigitNum(num)
+	}
+	if digits == 2 {
+		return cnv.MakeTwoDigitNum(num)
+	}
+	if digits == 3 {
+		return cnv.MakeThreeDigitNum(num)
+	}
+	return " "
+}
+
+func searchInNumbers(num string) bool {
+	for i := 1; i <= 10; i++ {
+		if num == numbers[0][i] || num == numbers[1][i] {
+			return true
+		}
+	}
+	return false
+}
+
+func (cnv *NumberConverter) MakeNumber() string {
+	if cnv.Num == 0 {
+		return numbers[0][0]
+	}
+	var numberStr []string
+	for {
+		if countsDigits(cnv.Num) >= 3 {
+			lastThreeDigitNumber := extractLastNDigit(cnv.Num, 3)
+			numberStr = append(numberStr, cnv.ReturnBase(lastThreeDigitNumber))
+			cnv.Num /= 1000
+		} else if digits := countsDigits(cnv.Num); digits < 3 && digits != 0 {
+			lastNDigitNumber := extractLastNDigit(cnv.Num, digits)
+			numberStr = append(numberStr, cnv.ReturnBase(lastNDigitNumber))
+			cnv.Num = 0
+		}
+		if cnv.Num == 0 {
+			break
+		}
+	}
+	var final string
+	for i := 0; i < len(numberStr); i++ {
+		if numberStr[i] != " " && manazl[i] != " " {
+			if numberStr[i] == numbers[0][1] || numberStr[i] == numbers[1][1] {
+				numberStr[i] = manazl[i]
+			} else if numberStr[i] == "اثنان" || numberStr[i] == "اثنين" || numberStr[i] == "اثنتين" || numberStr[i] == "اثنتان" {
+				numberStr[i] = manazl[i] + "ان"
+			} else if searchInNumbers(numberStr[i]) {
+				numberStr[i] += string(" ")
+				numberStr[i] += manazlInPlural[i]
+			} else {
+				numberStr[i] += string(" ")
+				numberStr[i] += manazl[i]
+			}
+		}
+	}
+	for i := len(numberStr) - 1; i >= 0; i-- {
+		if numberStr[i] == " " {
+			continue
+		}
+		if i+1 < len(numberStr) && numberStr[i+1] != " " {
+			numberStr[i] = "و" + numberStr[i]
+		}
+		final += " "
+		final += numberStr[i]
+	}
+
+	return strings.TrimSpace(final)
+}
+
+func main() {
+	// var index int
+	// var cnv NumberConverter
+	// file, err := os.Create("output.txt")
+	// if err != nil {
+	// 	fmt.Println("Error creating file:", err)
+	// 	return
+	// }
+	// defer file.Close() // Ensure the file is closed at the end
+
+	// // Write a string to the file
+	// for {
+	// 	fmt.Scan(&index)
+
+	// 	_, err = file.WriteString(cnv.MakeNumber(index) + "\n")
+	// 	if err != nil {
+	// 		fmt.Println("Error writing to file:", cnv.MakeNumber(index))
+	// 		return
+	// 	}
+
+	// 	fmt.Println("Data written to file successfully.")
+	// 	if index == 999 {
+	// 		break
+	// 	}
+	// }
+
+}
---
+++ tafqit_test.go
@@ -0,0 +1,240 @@
+package tafqit
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestMakeTens(t *testing.T) {
+	opt := Options{
+		Feminine:  false,
+		AG:        false,
+		Miah:      false,
+		SplitHund: false,
+		Billions:  false,
+	}
+	cnv := NumberConverter{
+		Opt: opt,
+		Num: 1,
+	}
+	require.Equal(t, "ثلاثون", cnv.MakeTens(3))
+	require.Equal(t, "أربعون", cnv.MakeTens(4))
+	require.Equal(t, "عشرة", cnv.MakeTens(1))
+	require.Equal(t, "عشرون", cnv.MakeTens(2))
+	require.Equal(t, "تسعون", cnv.MakeTens(9))
+	require.Equal(t, " ", cnv.MakeTens(0))
+	require.Equal(t, "خمسون", cnv.MakeTens(5))
+	cnv.Opt.AG = true
+	require.Equal(t, "ثلاثين", cnv.MakeTens(3))
+	require.Equal(t, "أربعين", cnv.MakeTens(4))
+	require.Equal(t, "عشرة", cnv.MakeTens(1))
+	require.Equal(t, "عشرين", cnv.MakeTens(2))
+	require.Equal(t, "تسعين", cnv.MakeTens(9))
+	require.Equal(t, "خمسين", cnv.MakeTens(5))
+}
+
+func TestHandleAG(t *testing.T) {
+	require.Equal(t, "عشرون", handleAG(10, false))
+	require.Equal(t, "عشرين", handleAG(10, true))
+	require.Equal(t, "ثلاثون", handleAG(3, false))
+	require.Equal(t, "ثلاثين", handleAG(3, true))
+	require.Equal(t, "أربعون", handleAG(4, false))
+	require.Equal(t, "أربعين", handleAG(4, true))
+	require.Equal(t, "خمسون", handleAG(5, false))
+	require.Equal(t, "خمسين", handleAG(5, true))
+}
+
+func TestHandleFeminine(t *testing.T) {
+	require.Equal(t, "اثنتان", handleFeminine(2, true, false))
+	require.Equal(t, "اثنان", handleFeminine(2, false, false))
+	require.Equal(t, "اثنتين", handleFeminine(2, true, true))
+	require.Equal(t, "اثنين", handleFeminine(2, false, true))
+	require.Equal(t, "واحدة", handleFeminine(1, true, false))
+	require.Equal(t, "واحد", handleFeminine(1, false, false))
+	require.Equal(t, "ثلاث", handleFeminine(3, true, false))
+	require.Equal(t, "ثلاثة", handleFeminine(3, false, false))
+	require.Equal(t, "أربع", handleFeminine(4, true, false))
+	require.Equal(t, "أربعة", handleFeminine(4, false, false))
+}
+
+func TestMakeOneDigit(t *testing.T) {
+	opt := Options{
+		Feminine:  true,
+		AG:        false,
+		Miah:      false,
+		SplitHund: false,
+		Billions:  false,
+	}
+	cnv := NumberConverter{
+		Opt: opt,
+		Num: 1,
+	}
+	require.Equal(t, " ", cnv.MakeOneDigitNum(0))
+	require.Equal(t, "اثنتان", cnv.MakeOneDigitNum(2))
+	require.Equal(t, "واحدة", cnv.MakeOneDigitNum(1))
+	require.Equal(t, "ثلاث", cnv.MakeOneDigitNum(3))
+	require.Equal(t, "أربع", cnv.MakeOneDigitNum(4))
+	cnv.Opt.Feminine = false
+	require.Equal(t, "ثلاثة", cnv.MakeOneDigitNum(3))
+	require.Equal(t, "أربعة", cnv.MakeOneDigitNum(4))
+	require.Equal(t, "اثنان", cnv.MakeOneDigitNum(2))
+	require.Equal(t, "واحد", cnv.MakeOneDigitNum(1))
+
+	cnv.Opt.AG = true
+	require.Equal(t, "اثنين", cnv.MakeOneDigitNum(2))
+	require.Equal(t, "واحد", cnv.MakeOneDigitNum(1))
+	require.Equal(t, "ثلاثة", cnv.MakeOneDigitNum(3))
+	require.Equal(t, "أربعة", cnv.MakeOneDigitNum(4))
+	cnv.Opt.Feminine = true
+	require.Equal(t, "اثنتين", cnv.MakeOneDigitNum(2))
+	require.Equal(t, "واحدة", cnv.MakeOneDigitNum(1))
+	require.Equal(t, "ثلاث", cnv.MakeOneDigitNum(3))
+	require.Equal(t, "أربع", cnv.MakeOneDigitNum(4))
+}
+
+func TestMakeTwoDigitNum(t *testing.T) {
+	opt := Options{
+		Feminine:  false,
+		AG:        false,
+		Miah:      false,
+		SplitHund: false,
+		Billions:  false,
+	}
+	cnv := NumberConverter{
+		Opt: opt,
+		Num: 1,
+	}
+	require.Equal(t, "واحد وخمسون", cnv.MakeTwoDigitNum(51))
+	require.Equal(t, "اثنان وثلاثون", cnv.MakeTwoDigitNum(32))
+	require.Equal(t, "خمسة وسبعون", cnv.MakeTwoDigitNum(75))
+	require.Equal(t, "اثنا عشر", cnv.MakeTwoDigitNum(12))
+	require.Equal(t, "أحد عشر", cnv.MakeTwoDigitNum(11))
+	require.Equal(t, "خمسة عشر", cnv.MakeTwoDigitNum(15))
+	require.Equal(t, "تسعة عشر", cnv.MakeTwoDigitNum(19))
+	require.Equal(t, "ثمانية وثمانون", cnv.MakeTwoDigitNum(88))
+	require.Equal(t, "تسعة وأربعون", cnv.MakeTwoDigitNum(49))
+	cnv.Opt.AG = true
+	require.Equal(t, "اثني عشر", cnv.MakeTwoDigitNum(12))
+	cnv.Opt.AG = false
+	cnv.Opt.Feminine = true
+	require.Equal(t, "اثنتا عشرة", cnv.MakeTwoDigitNum(12))
+	require.Equal(t, "احدى عشرة", cnv.MakeTwoDigitNum(11))
+	cnv.Opt.AG = true
+
+	require.Equal(t, "واحدة وخمسين", cnv.MakeTwoDigitNum(51))
+	require.Equal(t, "اثنتين وثلاثين", cnv.MakeTwoDigitNum(32))
+	require.Equal(t, "خمس وسبعين", cnv.MakeTwoDigitNum(75))
+	require.Equal(t, "اثنتي عشرة", cnv.MakeTwoDigitNum(12))
+	require.Equal(t, "احدى عشرة", cnv.MakeTwoDigitNum(11))
+	require.Equal(t, "خمس عشرة", cnv.MakeTwoDigitNum(15))
+	require.Equal(t, "تسع عشرة", cnv.MakeTwoDigitNum(19))
+	require.Equal(t, "ثمان وثمانين", cnv.MakeTwoDigitNum(88))
+	require.Equal(t, "تسع وأربعين", cnv.MakeTwoDigitNum(49))
+}
+
+func TestCountDigits(t *testing.T) {
+	require.Equal(t, 1, countsDigits(0))
+	require.Equal(t, 1, countsDigits(1))
+	require.Equal(t, 1, countsDigits(2))
+	require.Equal(t, 2, countsDigits(10))
+	require.Equal(t, 2, countsDigits(89))
+	require.Equal(t, 3, countsDigits(243))
+	require.Equal(t, 3, countsDigits(364))
+	require.Equal(t, 4, countsDigits(1234))
+	require.Equal(t, 5, countsDigits(12345))
+	require.Equal(t, 7, countsDigits(1234567))
+}
+
+func TestMakeThreeDigitNum(t *testing.T) {
+	opt := Options{
+		Feminine:  false,
+		AG:        false,
+		Miah:      false,
+		SplitHund: false,
+		Billions:  false,
+	}
+	cnv := NumberConverter{
+		Opt: opt,
+		Num: 1,
+	}
+	require.Equal(t, "مئة وثلاثة وأربعون", cnv.MakeThreeDigitNum(143))
+	require.Equal(t, "مئة", cnv.MakeThreeDigitNum(100))
+	require.Equal(t, "مئتان", cnv.MakeThreeDigitNum(200))
+	require.Equal(t, "ثلاث مئة وأربعة", cnv.MakeThreeDigitNum(304))
+	require.Equal(t, "خمس مئة", cnv.MakeThreeDigitNum(500))
+	require.Equal(t, "مئتان وستة وخمسون", cnv.MakeThreeDigitNum(256))
+	require.Equal(t, "ثمان مئة وخمسة وتسعون", cnv.MakeThreeDigitNum(895))
+	require.Equal(t, "ثمان مئة واثنا عشر", cnv.MakeThreeDigitNum(812))
+	require.Equal(t, "ثمان مئة وأحد عشر", cnv.MakeThreeDigitNum(811))
+	cnv.Opt.Feminine = true
+	require.Equal(t, "مئة وثلاث وأربعون", cnv.MakeThreeDigitNum(143))
+	require.Equal(t, "مئة", cnv.MakeThreeDigitNum(100))
+	require.Equal(t, "مئتان", cnv.MakeThreeDigitNum(200))
+	require.Equal(t, "ثلاث مئة وأربع", cnv.MakeThreeDigitNum(304))
+	require.Equal(t, "خمس مئة", cnv.MakeThreeDigitNum(500))
+	require.Equal(t, "مئتان وست وخمسون", cnv.MakeThreeDigitNum(256))
+	require.Equal(t, "ثمان مئة وخمس وتسعون", cnv.MakeThreeDigitNum(895))
+	require.Equal(t, "ثمان مئة واثنتا عشرة", cnv.MakeThreeDigitNum(812))
+	require.Equal(t, "ثمان مئة واحدى عشرة", cnv.MakeThreeDigitNum(811))
+	cnv.Opt.AG = true
+	require.Equal(t, "مئة وثلاث وأربعين", cnv.MakeThreeDigitNum(143))
+	require.Equal(t, "مئة", cnv.MakeThreeDigitNum(100))
+	require.Equal(t, "مئتين", cnv.MakeThreeDigitNum(200))
+	require.Equal(t, "ثلاث مئة وأربع", cnv.MakeThreeDigitNum(304))
+	require.Equal(t, "خمس مئة", cnv.MakeThreeDigitNum(500))
+	require.Equal(t, "مئتين وست وخمسين", cnv.MakeThreeDigitNum(256))
+	require.Equal(t, "ثمان مئة وخمس وتسعين", cnv.MakeThreeDigitNum(895))
+	require.Equal(t, "ثمان مئة واثنتي عشرة", cnv.MakeThreeDigitNum(812))
+	require.Equal(t, "ثمان مئة واحدى عشرة", cnv.MakeThreeDigitNum(811))
+
+}
+
+func TestReturnLastNDigit(t *testing.T) {
+	require.Equal(t, 765, extractLastNDigit(1234765, 3))
+	require.Equal(t, 7659, extractLastNDigit(12347659, 4))
+}
+func TestSearchInNumbers(t *testing.T) {
+	require.Equal(t, true, searchInNumbers("واحد"))
+	require.Equal(t, true, searchInNumbers("واحدة"))
+	require.Equal(t, true, searchInNumbers("عشرة"))
+	require.Equal(t, true, searchInNumbers("ثلاثة"))
+	require.Equal(t, false, searchInNumbers("تين"))
+	require.Equal(t, false, searchInNumbers("مربع"))
+}
+
+func TestReturnBase(t *testing.T) {
+	opt := Options{
+		Feminine:  false,
+		AG:        false,
+		Miah:      false,
+		SplitHund: false,
+		Billions:  false,
+	}
+	cnv := NumberConverter{
+		Opt: opt,
+		Num: 1,
+	}
+	require.Equal(t, "مئة وثلاثة وأربعون", cnv.ReturnBase(143))
+	require.Equal(t, "واحد وخمسون", cnv.ReturnBase(51))
+	require.Equal(t, "ثلاثة", cnv.ReturnBase(3))
+	require.Equal(t, " ", cnv.ReturnBase(1234))
+
+}
+
+func TestMakeNumber(t *testing.T) {
+	opt := Options{
+		Feminine:  false,
+		AG:        false,
+		Miah:      false,
+		SplitHund: false,
+		Billions:  false,
+	}
+	cnv := NumberConverter{
+		Opt: opt,
+		Num: 1650212,
+	}
+	require.Equal(t, "مليون وستة مئة وخمسون ألف ومئتان واثنا عشر", cnv.MakeNumber())
+	cnv.Num = 49800
+	require.Equal(t, "تسعة وأربعون ألف وثمانية مئة", cnv.MakeNumber())
+}
